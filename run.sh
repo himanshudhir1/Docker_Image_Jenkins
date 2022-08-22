ec6a1097f20b91cb030f3f3f291899c12245d6e8
himanshudhir1 <111078795+himanshudhir1@users.noreply.github.com>
2022-08-22 16:14:19 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 #!/bin/bash -e
 #define variables to store info
 version=5.3
-scriptname="CSVSample"
+scriptname="LoadTest_Jmeter_Jenkins_Integration"
 # override the HEAP settings and run the jmeter script.
 JVM_ARGS="-Xms512m -Xmx2048m" jmeter -n -t /opt/apache-jmeter-${version}/bin/examples/${scriptname}.jmx -l /jmeter.jtl 2>&1
 java -jar /opt/apache-jmeter-${version}/lib/cmdrunner-2.2.jar --tool Reporter --plugin-type AggregateReport --input-jtl /jmeter.jtl --generate-csv /results/results.csv 2>&1
